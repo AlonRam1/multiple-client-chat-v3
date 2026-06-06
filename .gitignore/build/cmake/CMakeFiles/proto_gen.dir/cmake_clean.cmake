720d49271fb5cf232961ab3c82f3c75c30989d01
AlonRam1 <alon.ram.rimmler@gmail.com>
2026-06-07 01:23:16 +0300

    created gitignore folder

---
+++ .gitignore/build/cmake/CMakeFiles/proto_gen.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "/home/alon/Desktop/personal projects/multiple-client-chat-v3/build/gen/grpc/chat.grpc.pb.cc"
+  "/home/alon/Desktop/personal projects/multiple-client-chat-v3/build/gen/grpc/chat.grpc.pb.h"
+  "/home/alon/Desktop/personal projects/multiple-client-chat-v3/build/gen/protobuf/chat.pb.cc"
+  "/home/alon/Desktop/personal projects/multiple-client-chat-v3/build/gen/protobuf/chat.pb.h"
+  "CMakeFiles/proto_gen.dir/build/gen/grpc/chat.grpc.pb.cc.o"
+  "CMakeFiles/proto_gen.dir/build/gen/grpc/chat.grpc.pb.cc.o.d"
+  "CMakeFiles/proto_gen.dir/build/gen/protobuf/chat.pb.cc.o"
+  "CMakeFiles/proto_gen.dir/build/gen/protobuf/chat.pb.cc.o.d"
+  "libproto_gen.a"
+  "libproto_gen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proto_gen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ .gitignore/build/cmake/server/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "/home/alon/Desktop/personal projects/multiple-client-chat-v3/build/bin/server/server"
+  "/home/alon/Desktop/personal projects/multiple-client-chat-v3/build/bin/server/server.pdb"
+  "CMakeFiles/server.dir/link.d"
+  "CMakeFiles/server.dir/src/server.cc.o"
+  "CMakeFiles/server.dir/src/server.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
